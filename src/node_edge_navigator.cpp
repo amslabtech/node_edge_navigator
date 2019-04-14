7e3edc4212b8e635704314b03aca823908266a5f
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-04-14 16:07:24 +0900

    update

--- src/node_edge_navigator.cpp
+++ src/node_edge_navigator.cpp
@@ -19,8 +19,13 @@ public:
 	void path_callback(const std_msgs::Int32MultiArrayConstPtr&);
 	void pose_callback(const geometry_msgs::PoseStampedConstPtr&);
 	void edge_callback(const amsl_navigation_msgs::EdgeConstPtr&);
+	void process(void);
+	void get_node_from_id(int, amsl_navigation_msgs::Node&);
+	double pi_2_pi(double);
 
 private:
+	double HZ;
+
 	ros::NodeHandle nh;
 	ros::NodeHandle private_nh;
 
@@ -32,13 +37,24 @@ private:
 
 	tf::TransformListener listener;
 	tf::StampedTransform transform;
+
+	amsl_navigation_msgs::NodeEdgeMap map;
+	std::vector<int> global_path_ids;
+	geometry_msgs::PoseStamped estimated_pose;
+	amsl_navigation_msgs::Edge edge;
+
+	bool map_subscribed;
+	bool global_path_subscribed;
+	bool pose_updated;
+	bool edge_updated;
+	std::vector<int> passed_paths;
 };
 
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "node_edge_navigator");
 	NodeEdgeNavigator node_edge_navigator;
-	ros::spin();
+	node_edge_navigator.process();
 	return 0;
 }
 
@@ -51,25 +67,87 @@ NodeEdgeNavigator::NodeEdgeNavigator(void)
 	pose_sub = nh.subscribe("/estimated_pose/pose", 1, &NodeEdgeNavigator::pose_callback, this);
 	edge_sub = nh.subscribe("/estimated_pose/edge", 1, &NodeEdgeNavigator::edge_callback, this);
 
+	private_nh.param("HZ", HZ, {50});
+
+	map_subscribed = false;
+	global_path_subscribed = false;
+	pose_updated = false;
+	edge_updated = false;
+
 	std::cout << "=== node_edge_navigator ===" << std::endl;
+	std::cout << "HZ: " << HZ << std::endl;
 }
 
 void NodeEdgeNavigator::map_callback(const amsl_navigation_msgs::NodeEdgeMapConstPtr& msg)
 {
-
+	map = *msg;
+	map_subscribed = true;
 }
 
 void NodeEdgeNavigator::path_callback(const std_msgs::Int32MultiArrayConstPtr& msg)
 {
-
+	global_path_ids = msg->data;
+	global_path_subscribed = true;
 }
 
 void NodeEdgeNavigator::pose_callback(const geometry_msgs::PoseStampedConstPtr& msg)
 {
-
+	estimated_pose = *msg;
+	pose_updated = true;
 }
 
 void NodeEdgeNavigator::edge_callback(const amsl_navigation_msgs::EdgeConstPtr& msg)
 {
+	edge = *msg;
+	edge_updated = true;
+}
 
+void NodeEdgeNavigator::process(void)
+{
+	ros::Rate loop_rate(HZ);
+	while(ros::ok()){
+		if(map_subscribed && global_path_subscribed){
+			if(pose_updated && edge_updated){
+				if(global_path_ids[0] == edge.node0_id){
+					// arrived 
+					global_path_ids.erase(global_path_ids.begin());
+				}
+				if(global_path_ids.empty()){
+					// goal
+				}
+				amsl_navigation_msgs::Node target_node;
+				get_node_from_id(global_path_ids[0], target_node);
+				// caluculate target node direction
+				double global_node_direction = atan2(target_node.point.y - estimated_pose.pose.position.y, target_node.point.x - estimated_pose.pose.position.x);
+				double target_node_direction = global_node_direction - tf::getYaw(estimated_pose.pose.orientation);
+				target_node_direction = pi_2_pi(target_node_direction);
+				geometry_msgs::PoseStamped direction;
+				direction.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0, 0, target_node_direction);
+				direction.pose.position = estimated_pose.pose.position;
+				direction.header = estimated_pose.header; 
+				direction_pub.publish(direction);
+				// excess detection
+
+				pose_updated = false;
+				edge_updated = false;
+			}
+		}
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+}
+
+void NodeEdgeNavigator::get_node_from_id(int id, amsl_navigation_msgs::Node& node)
+{
+	for(auto n : map.nodes){
+		if(n.id == id){
+			node = n;
+			return;
+		}
+	}
+}
+
+double NodeEdgeNavigator::pi_2_pi(double angle)
+{
+	return atan2(sin(angle), cos(angle));
 }
