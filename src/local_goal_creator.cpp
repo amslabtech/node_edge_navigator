155a972c780fe2bea6db6d8763b22496c3dd5cba
Jumpei Arima <arijun0307@gmail.com>
2019-04-24 11:21:50 +0900

    issues #4(detect local goal  without localmap)

--- src/local_goal_creator.cpp
+++ src/local_goal_creator.cpp
@@ -1,202 +1,64 @@
 #include <ros/ros.h>
-
-#include <tf/tf.h>
-#include <tf/transform_listener.h>
-
-#include <std_msgs/Int32MultiArray.h>
+#include <nav_msgs/OccupancyGrid.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <std_msgs/Float64.h>
+#include <tf/tf.h>
 
-#include "amsl_navigation_msgs/Node.h"
-#include "amsl_navigation_msgs/Edge.h"
-#include "amsl_navigation_msgs/NodeEdgeMap.h"
-#include "amsl_navigation_msgs/UpdateNode.h"
+nav_msgs::OccupancyGrid local_map;
+bool map_received = false;
+bool target_received = false;
+float target_orientation = 0.0;
+double longest_path_length_angle = 0.0;
 
-class NodeEdgeNavigator
+void TargetCallback(const std_msgs::Float64ConstPtr& msg)
 {
-public:
-	NodeEdgeNavigator(void);
+  target_orientation = msg->data;
+  target_received = true;
+}
 
-	void map_callback(const amsl_navigation_msgs::NodeEdgeMapConstPtr&);
-	void path_callback(const std_msgs::Int32MultiArrayConstPtr&);
-	void pose_callback(const geometry_msgs::PoseStampedConstPtr&);
-	void edge_callback(const amsl_navigation_msgs::EdgeConstPtr&);
-	void process(void);
-	void get_node_from_id(int, amsl_navigation_msgs::Node&);
-	double pi_2_pi(double);
-	void request_replanning(void);
+void MapCallback(const nav_msgs::OccupancyGridConstPtr& msg)
+{
+  local_map = *msg;
+  map_received = true;
+}
 
-private:
-	double HZ;
-	double EXCESS_DETECTION_RATIO;
+void detection_main(geometry_msgs::PoseStamped& goal)
+{
+	goal.header = local_map.header;
+	goal.pose.position.x = 5.0*cos(target_orientation); 
+	goal.pose.position.y = 5.0*sin(target_orientation); 
+	goal.pose.position.z = 0.0;
+	goal.pose.orientation = tf::createQuaternionMsgFromYaw(target_orientation);
+}
 
-	ros::NodeHandle nh;
-	ros::NodeHandle private_nh;
+void LocalGoalCreator()
+{
+  ros::NodeHandle nh;
+  ros::Subscriber sub_map = nh.subscribe("/local_map", 1, MapCallback);
+  ros::Subscriber sub_target = nh.subscribe("/direction/relative", 1, TargetCallback);
+  
+  ros::Publisher pub_goal = nh.advertise<geometry_msgs::PoseStamped>("/local_goal",1);
 
-	ros::Publisher direction_pub;
-	ros::Subscriber map_sub;
-	ros::Subscriber path_sub;
-	ros::Subscriber pose_sub;
-	ros::Subscriber edge_sub;
-	ros::ServiceClient node_client; 
+  geometry_msgs::PoseStamped local_goal;
 
-	tf::TransformListener listener;
-	tf::StampedTransform transform;
-
-	amsl_navigation_msgs::NodeEdgeMap map;
-	std::vector<int> global_path_ids;
-	geometry_msgs::PoseStamped estimated_pose;
-	amsl_navigation_msgs::Edge edge;
-
-	bool map_subscribed;
-	bool global_path_subscribed;
-	bool pose_updated;
-	bool edge_updated;
-	std::vector<int> passed_paths;
-};
+  ros::Rate loop_rate(10);
+  while(ros::ok()){
+	if(map_received && target_received){
+	  detection_main(local_goal);
+	  pub_goal.publish(local_goal);
+	}
+	else{
+	  std::cout << "map:" << map_received << " target:" << target_received << std::endl;
+	}
+	ros::spinOnce();
+	loop_rate.sleep();
+  }
+}
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "node_edge_navigator");
-	NodeEdgeNavigator node_edge_navigator;
-	node_edge_navigator.process();
-	return 0;
-}
-
-NodeEdgeNavigator::NodeEdgeNavigator(void)
-	: private_nh("~")
-{
-	direction_pub = nh.advertise<geometry_msgs::PoseStamped>("/direction/relative", 1);
-	map_sub = nh.subscribe("/node_edge_map", 1, &NodeEdgeNavigator::map_callback, this);
-	path_sub = nh.subscribe("/global_path", 1, &NodeEdgeNavigator::path_callback, this);
-	pose_sub = nh.subscribe("/estimated_pose/pose", 1, &NodeEdgeNavigator::pose_callback, this);
-	edge_sub = nh.subscribe("/estimated_pose/edge", 1, &NodeEdgeNavigator::edge_callback, this);
-	node_client = nh.serviceClient<amsl_navigation_msgs::UpdateNode>("/node_edge_map/update_node");
-
-	private_nh.param("HZ", HZ, {50});
-	private_nh.param("EXCESS_DETECTION_RATIO", EXCESS_DETECTION_RATIO, {1.2});
-
-	map_subscribed = false;
-	global_path_subscribed = false;
-	pose_updated = false;
-	edge_updated = false;
-
-	std::cout << "=== node_edge_navigator ===" << std::endl;
-	std::cout << "HZ: " << HZ << std::endl;
-}
-
-void NodeEdgeNavigator::map_callback(const amsl_navigation_msgs::NodeEdgeMapConstPtr& msg)
-{
-	map = *msg;
-	map_subscribed = true;
-}
-
-void NodeEdgeNavigator::path_callback(const std_msgs::Int32MultiArrayConstPtr& msg)
-{
-	global_path_ids = msg->data;
-	global_path_subscribed = true;
-}
-
-void NodeEdgeNavigator::pose_callback(const geometry_msgs::PoseStampedConstPtr& msg)
-{
-	estimated_pose = *msg;
-	pose_updated = true;
-}
-
-void NodeEdgeNavigator::edge_callback(const amsl_navigation_msgs::EdgeConstPtr& msg)
-{
-	edge = *msg;
-	edge_updated = true;
-}
-
-void NodeEdgeNavigator::process(void)
-{
-	ros::Rate loop_rate(HZ);
-	while(ros::ok()){
-		if(map_subscribed && global_path_subscribed){
-			if(pose_updated && edge_updated){
-				if(global_path_ids[0] == edge.node0_id){
-					// arrived 
-					global_path_ids.erase(global_path_ids.begin());
-				}
-				if(global_path_ids.empty()){
-					// goal
-				}
-				geometry_msgs::PoseStamped direction;
-				amsl_navigation_msgs::Node target_node;
-				get_node_from_id(global_path_ids[0], target_node);
-				if((target_node.type == "coordinate") || (target_node.type == "gps")){
-					// caluculate target node direction
-					double global_node_direction = atan2(target_node.point.y - estimated_pose.pose.position.y, target_node.point.x - estimated_pose.pose.position.x);
-					double target_node_direction = global_node_direction - tf::getYaw(estimated_pose.pose.orientation);
-					target_node_direction = pi_2_pi(target_node_direction);
-					direction.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0, 0, target_node_direction);
-				}else if(target_node.type == "intersection"){
-					// caluculate target node direction
-					amsl_navigation_msgs::Node last_node;
-					get_node_from_id(edge.node0_id, last_node);
-					double global_node_direction = atan2(target_node.point.y - last_node.point.y, target_node.point.x - last_node.point.x);
-					double target_node_direction = global_node_direction - tf::getYaw(estimated_pose.pose.orientation);
-					target_node_direction = pi_2_pi(target_node_direction);
-					direction.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0, 0, target_node_direction);
-					// excess detection
-					request_replanning();
-				}else{
-					// default
-					double global_node_direction = atan2(target_node.point.y - estimated_pose.pose.position.y, target_node.point.x - estimated_pose.pose.position.x);
-					double target_node_direction = global_node_direction - tf::getYaw(estimated_pose.pose.orientation);
-					target_node_direction = pi_2_pi(target_node_direction);
-					direction.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0, 0, target_node_direction);
-				}
-				direction.pose.position = estimated_pose.pose.position;
-				direction.header = estimated_pose.header; 
-				direction_pub.publish(direction);
-
-				pose_updated = false;
-				edge_updated = false;
-			}
-		}
-		ros::spinOnce();
-		loop_rate.sleep();
-	}
-}
-
-void NodeEdgeNavigator::get_node_from_id(int id, amsl_navigation_msgs::Node& node)
-{
-	for(auto n : map.nodes){
-		if(n.id == id){
-			node = n;
-			return;
-		}
-	}
-}
-
-double NodeEdgeNavigator::pi_2_pi(double angle)
-{
-	return atan2(sin(angle), cos(angle));
-}
-
-void NodeEdgeNavigator::request_replanning(void)
-{
-	if(edge.progress >= EXCESS_DETECTION_RATIO){
-		amsl_navigation_msgs::Node node;
-		int id = 1000;
-		for(auto it=map.nodes.begin();it!=map.nodes.end();++it){
-			if(it->id != id){
-				break;
-			}
-			id++;
-		}
-		node.id = id;
-		node.label = "replanning";
-		node.type = "coordinate";
-		node.point = estimated_pose.pose.position;
-		amsl_navigation_msgs::UpdateNode node_service;
-		node_service.request.node = node;
-		node_service.request.operation = node_service.request.ADD;
-		if(node_client.call(node_service)){
-			std::cout << "node added to map" << std::endl;
-		}else{
-			std::cout << "failed to call service" << std::endl;
-		}
-	}
+  ros::init(argc, argv, "local_goal_creator");
+  LocalGoalCreator();
+  ROS_INFO("Killing now!!!!!");
+  return 0;
 }
