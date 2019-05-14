fe1baec44ef4f9d19f270e3a208a83068a0b6ebf
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-05-14 14:53:46 +0900

    update for #9

--- src/node_edge_navigator.cpp
+++ src/node_edge_navigator.cpp
@@ -3,6 +3,7 @@
 #include <tf/tf.h>
 #include <tf/transform_listener.h>
 
+#include <std_msgs/Bool.h>
 #include <std_msgs/Int32MultiArray.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <nav_msgs/Odometry.h>
@@ -23,10 +24,12 @@ public:
 	void path_callback(const std_msgs::Int32MultiArrayConstPtr&);
 	void pose_callback(const nav_msgs::OdometryConstPtr&);
 	void edge_callback(const amsl_navigation_msgs::EdgeConstPtr&);
+	void intersection_flag_callback(const std_msgs::BoolConstPtr&);
 	void process(void);
 	void get_node_from_id(int, amsl_navigation_msgs::Node&);
 	double pi_2_pi(double);
 	void request_replanning(void);
+	void arrived_at_node(void);
 
 private:
 	double HZ;
@@ -40,6 +43,7 @@ private:
 	ros::Subscriber path_sub;
 	ros::Subscriber pose_sub;
 	ros::Subscriber edge_sub;
+	ros::Subscriber intersection_flag_sub;
 	ros::ServiceClient node_client; 
 	ros::ServiceClient edge_client; 
 	ros::ServiceClient replan_client;
@@ -57,6 +61,7 @@ private:
 	bool pose_updated;
 	bool edge_updated;
 	std::vector<int> passed_paths;
+	bool intersection_flag;
 };
 
 int main(int argc, char** argv)
@@ -75,6 +80,7 @@ NodeEdgeNavigator::NodeEdgeNavigator(void)
 	path_sub = nh.subscribe("/global_path", 1, &NodeEdgeNavigator::path_callback, this);
 	pose_sub = nh.subscribe("/estimated_pose/pose", 1, &NodeEdgeNavigator::pose_callback, this);
 	edge_sub = nh.subscribe("/estimated_pose/edge", 1, &NodeEdgeNavigator::edge_callback, this);
+	intersection_flag_sub = nh.subscribe("/intersection_flag", 1, &NodeEdgeNavigator::intersection_flag_callback, this);
 	node_client = nh.serviceClient<amsl_navigation_msgs::UpdateNode>("/node_edge_map/update_node");
 	edge_client = nh.serviceClient<amsl_navigation_msgs::UpdateEdge>("/node_edge_map/update_edge");
 	replan_client = nh.serviceClient<amsl_navigation_msgs::Replan>("/global_path/replan");
@@ -97,6 +103,7 @@ void NodeEdgeNavigator::map_callback(const amsl_navigation_msgs::NodeEdgeMapCons
 {
 	map = *msg;
 	map_subscribed = true;
+	std::cout << "received map" << std::endl;
 }
 
 void NodeEdgeNavigator::path_callback(const std_msgs::Int32MultiArrayConstPtr& msg)
@@ -117,6 +124,12 @@ void NodeEdgeNavigator::edge_callback(const amsl_navigation_msgs::EdgeConstPtr& 
 	edge_updated = true;
 }
 
+void NodeEdgeNavigator::intersection_flag_callback(const std_msgs::BoolConstPtr& msg)
+{
+	std::cout << "intersection" << std::endl;
+	intersection_flag = msg->data;
+}
+
 void NodeEdgeNavigator::process(void)
 {
 	ros::Rate loop_rate(HZ);
@@ -126,10 +139,6 @@ void NodeEdgeNavigator::process(void)
 			if(pose_updated && edge_updated){
 				std::cout << "=== node_edge_navigator ===" << std::endl;
 				double start_time = ros::Time::now().toSec();
-				if(global_path_ids[0] == edge.node0_id){
-					// arrived 
-					global_path_ids.erase(global_path_ids.begin());
-				}
 				if(global_path_ids.empty()){
 					// goal
 				}
@@ -144,6 +153,12 @@ void NodeEdgeNavigator::process(void)
 					target_node_direction = pi_2_pi(target_node_direction);
 					direction.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0, 0, target_node_direction);
 				}else if(target_node.type == "intersection"){
+					if(intersection_flag){
+						intersection_flag = false;
+						arrived_at_node();
+						// update target node
+						get_node_from_id(global_path_ids[0], target_node);
+					}
 					// caluculate target node direction
 					amsl_navigation_msgs::Node last_node;
 					get_node_from_id(edge.node0_id, last_node);
@@ -164,6 +179,7 @@ void NodeEdgeNavigator::process(void)
 					target_node_direction = pi_2_pi(target_node_direction);
 					direction.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0, 0, target_node_direction);
 				}
+				std::cout << "target node:\n" << target_node << std::endl;
 				std::cout << "direction: " << tf::getYaw(direction.pose.orientation) << "[rad]" << std::endl;
 				// direction.pose.position = estimated_pose.pose.pose.position;
 				direction.header.frame_id = "base_link"; 
@@ -241,4 +257,13 @@ void NodeEdgeNavigator::request_replanning(void)
 		std::cout << "failed to call service" << std::endl;
 		return;
 	}
+}
+
+void NodeEdgeNavigator::arrived_at_node(void)
+{
+	amsl_navigation_msgs::Node node;
+	get_node_from_id(*(global_path_ids.begin()), node);
+	std::cout << "arrived at node: \n" << node << std::endl;
+	global_path_ids.erase(global_path_ids.begin());
+	std::cout << "the begin element of global path was deleted" << std::endl;
 }
