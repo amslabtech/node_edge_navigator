2dc71b3e58ae9209d86dbe86b68a6587dbada2ba
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-05-19 21:40:58 +0900

    update

---
+++ test/node_edge_navigator_test.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+
+#include <ros/ros.h>
+
+TEST(TestSuite, test_navigator1)
+{
+	ros::NodeHandle nh;
+	EXPECT_EQ(M_PI, M_PI);
+}
+
+int main(int argc, char** argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+
+	ros::init(argc, argv, "node_edge_navigator_test");
+
+	ros::AsyncSpinner spinner(1);
+	spinner.start();
+
+	int r_e_t = RUN_ALL_TESTS();
+
+	spinner.stop();
+
+	ros::shutdown();
+
+	return r_e_t;
+}
