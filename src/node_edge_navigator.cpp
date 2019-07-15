30d32ec47318ccd4d954640de3ba8ebac7e96653
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-07-15 12:42:05 +0900

    move empty check

--- src/node_edge_navigator.cpp
+++ src/node_edge_navigator.cpp
@@ -157,98 +157,99 @@ void NodeEdgeNavigator::process(void)
             if(pose_updated && edge_updated){
                 std::cout << "=== node_edge_navigator ===" << std::endl;
                 double start_time = ros::Time::now().toSec();
-                if(global_path_ids.empty()){
-                    // goal
-                    std::cout << "global path is empty" << std::endl;
-                }
-                if(global_path_ids.size() > 1){
-                    if(global_path_ids[0] != estimated_edge.node1_id){
-                        if(global_path_ids[0] == estimated_edge.node0_id && global_path_ids[1] == estimated_edge.node1_id){
-                            std::cout << "global path[0] was deleted because robot was already on the target edge" << std::endl;
-                            last_target_node_id = global_path_ids[0];
-                            global_path_ids.erase(global_path_ids.begin());
-                        }else if(global_path_ids[0] != estimated_edge.node0_id && global_path_ids[1] != estimated_edge.node1_id){
-                            std::cout << "maybe localization error" << std::endl;
-                            std::cout << estimated_edge.node0_id << ", " << estimated_edge.node1_id << std::endl;
-                            std::cout << global_path_ids[0] << ", " << global_path_ids[1] << std::endl;
-                            int global_path_size = global_path_ids.size();
-                            for(int i=0;i<global_path_size-1;i++){
-                                if(global_path_ids[i] == estimated_edge.node0_id && global_path_ids[i+1] == estimated_edge.node1_id){
-                                    std::cout << "estimated edge was found in global path" << std::endl;
-                                    global_path_ids.erase(global_path_ids.begin(), global_path_ids.begin() + i);
-                                    break;
+                if(!global_path_ids.empty()){
+                    if(global_path_ids.size() > 1){
+                        if(global_path_ids[0] != estimated_edge.node1_id){
+                            if(global_path_ids[0] == estimated_edge.node0_id && global_path_ids[1] == estimated_edge.node1_id){
+                                std::cout << "global path[0] was deleted because robot was already on the target edge" << std::endl;
+                                last_target_node_id = global_path_ids[0];
+                                global_path_ids.erase(global_path_ids.begin());
+                            }else if(global_path_ids[0] != estimated_edge.node0_id && global_path_ids[1] != estimated_edge.node1_id){
+                                std::cout << "maybe localization error" << std::endl;
+                                std::cout << estimated_edge.node0_id << ", " << estimated_edge.node1_id << std::endl;
+                                std::cout << global_path_ids[0] << ", " << global_path_ids[1] << std::endl;
+                                int global_path_size = global_path_ids.size();
+                                for(int i=0;i<global_path_size-1;i++){
+                                    if(global_path_ids[i] == estimated_edge.node0_id && global_path_ids[i+1] == estimated_edge.node1_id){
+                                        std::cout << "estimated edge was found in global path" << std::endl;
+                                        global_path_ids.erase(global_path_ids.begin(), global_path_ids.begin() + i);
+                                        break;
+                                    }
                                 }
                             }
                         }
                     }
-                }
-                geometry_msgs::PoseStamped direction;
-                amsl_navigation_msgs::Node target_node;
-                get_node_from_id(global_path_ids[0], target_node);
-                std::cout << "global path id: " << std::endl;
-                for(auto gp : global_path_ids){
-                    std::cout << gp << std::endl;
-                }
-                std::cout << "target node: \n" << target_node << std::endl;
-                if((target_node.type == "position") || (target_node.type == "gps")){
-                    double distance = get_distance_from_points(target_node.point, estimated_pose.pose.pose.position);
-                    if(distance <= GOAL_RADIUS){
-                        arrived_at_node();
-                        // update target node
-                        get_node_from_id(global_path_ids[0], target_node);
+                    geometry_msgs::PoseStamped direction;
+                    amsl_navigation_msgs::Node target_node;
+                    get_node_from_id(global_path_ids[0], target_node);
+                    std::cout << "global path id: " << std::endl;
+                    for(auto gp : global_path_ids){
+                        std::cout << gp << std::endl;
                     }
-                    // caluculate target node direction
-                    double global_node_direction = atan2(target_node.point.y - estimated_pose.pose.pose.position.y, target_node.point.x - estimated_pose.pose.pose.position.x);
-                    double target_node_direction = global_node_direction - tf::getYaw(estimated_pose.pose.pose.orientation);
-                    target_node_direction = pi_2_pi(target_node_direction);
-                    direction.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0, 0, target_node_direction);
-                }else if(target_node.type == "intersection"){
-                    amsl_navigation_msgs::Node last_target_node;
-                    get_node_from_id(last_target_node_id, last_target_node);
-                    std::cout << "last target node: \n" << last_target_node << std::endl;
-                    if(intersection_flag){
-                        intersection_flag = false;
-                        if(estimated_edge.progress > INTERSECTION_ACCEPTANCE_PROGRESS_RATIO){
-                            if(last_target_node_id != estimated_edge.node1_id){
-                                std::cout << "new intersection" << std::endl;
-                                last_target_node_id = global_path_ids[0];
-                                get_node_from_id(last_target_node_id, last_target_node);
-                                arrived_at_node();
-                                // update target node
-                                get_node_from_id(global_path_ids[0], target_node);
-                            }else{
-                                std::cout << "already arrived at the intersection" << std::endl;
+                    std::cout << "target node: \n" << target_node << std::endl;
+                    if((target_node.type == "position") || (target_node.type == "gps")){
+                        double distance = get_distance_from_points(target_node.point, estimated_pose.pose.pose.position);
+                        if(distance <= GOAL_RADIUS){
+                            arrived_at_node();
+                            // update target node
+                            get_node_from_id(global_path_ids[0], target_node);
+                        }
+                        // caluculate target node direction
+                        double global_node_direction = atan2(target_node.point.y - estimated_pose.pose.pose.position.y, target_node.point.x - estimated_pose.pose.pose.position.x);
+                        double target_node_direction = global_node_direction - tf::getYaw(estimated_pose.pose.pose.orientation);
+                        target_node_direction = pi_2_pi(target_node_direction);
+                        direction.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0, 0, target_node_direction);
+                    }else if(target_node.type == "intersection"){
+                        amsl_navigation_msgs::Node last_target_node;
+                        get_node_from_id(last_target_node_id, last_target_node);
+                        std::cout << "last target node: \n" << last_target_node << std::endl;
+                        if(intersection_flag){
+                            intersection_flag = false;
+                            if(estimated_edge.progress > INTERSECTION_ACCEPTANCE_PROGRESS_RATIO){
+                                if(last_target_node_id != estimated_edge.node1_id){
+                                    std::cout << "new intersection" << std::endl;
+                                    last_target_node_id = global_path_ids[0];
+                                    get_node_from_id(last_target_node_id, last_target_node);
+                                    arrived_at_node();
+                                    // update target node
+                                    get_node_from_id(global_path_ids[0], target_node);
+                                }else{
+                                    std::cout << "already arrived at the intersection" << std::endl;
+                                }
                             }
                         }
+                        // caluculate target node direction
+                        double global_node_direction = atan2(target_node.point.y - last_target_node.point.y, target_node.point.x - last_target_node.point.x);
+                        std::cout << "edge direction: " << global_node_direction << "[rad]" << std::endl;
+                        double target_node_direction = global_node_direction - tf::getYaw(estimated_pose.pose.pose.orientation);
+                        target_node_direction = pi_2_pi(target_node_direction);
+                        direction.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0, 0, target_node_direction);
+                        // excess detection
+                        if((estimated_edge.progress >= EXCESS_DETECTION_RATIO) && ENABLE_REQUESTING_REPLANNING){
+                            std::cout << "intersection excession is detected" << std::endl;
+                            request_replanning();
+                        }
+                    }else{
+                        // default
+                        double global_node_direction = atan2(target_node.point.y - estimated_pose.pose.pose.position.y, target_node.point.x - estimated_pose.pose.pose.position.x);
+                        double target_node_direction = global_node_direction - tf::getYaw(estimated_pose.pose.pose.orientation);
+                        target_node_direction = pi_2_pi(target_node_direction);
+                        direction.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0, 0, target_node_direction);
                     }
-                    // caluculate target node direction
-                    double global_node_direction = atan2(target_node.point.y - last_target_node.point.y, target_node.point.x - last_target_node.point.x);
-                    std::cout << "edge direction: " << global_node_direction << "[rad]" << std::endl;
-                    double target_node_direction = global_node_direction - tf::getYaw(estimated_pose.pose.pose.orientation);
-                    target_node_direction = pi_2_pi(target_node_direction);
-                    direction.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0, 0, target_node_direction);
-                    // excess detection
-                    if((estimated_edge.progress >= EXCESS_DETECTION_RATIO) && ENABLE_REQUESTING_REPLANNING){
-                        std::cout << "intersection excession is detected" << std::endl;
-                        request_replanning();
-                    }
-                }else{
-                    // default
-                    double global_node_direction = atan2(target_node.point.y - estimated_pose.pose.pose.position.y, target_node.point.x - estimated_pose.pose.pose.position.x);
-                    double target_node_direction = global_node_direction - tf::getYaw(estimated_pose.pose.pose.orientation);
-                    target_node_direction = pi_2_pi(target_node_direction);
-                    direction.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0, 0, target_node_direction);
-                }
-                //std::cout << "target node:\n" << target_node << std::endl;
-                std::cout << "direction: " << tf::getYaw(direction.pose.orientation) << "[rad]" << std::endl;
-                // direction.pose.position = estimated_pose.pose.pose.position;
-                direction.header.frame_id = "base_link";
-                direction.header.stamp = ros::Time::now();
-                direction_pub.publish(direction);
+                    //std::cout << "target node:\n" << target_node << std::endl;
+                    std::cout << "direction: " << tf::getYaw(direction.pose.orientation) << "[rad]" << std::endl;
+                    // direction.pose.position = estimated_pose.pose.pose.position;
+                    direction.header.frame_id = "base_link";
+                    direction.header.stamp = ros::Time::now();
+                    direction_pub.publish(direction);
 
-                pose_updated = false;
-                edge_updated = false;
-                std::cout << "process time: " << ros::Time::now().toSec() - start_time << "[s]" << std::endl;
+                    pose_updated = false;
+                    edge_updated = false;
+                    std::cout << "process time: " << ros::Time::now().toSec() - start_time << "[s]" << std::endl;
+                }else{
+                    // goal
+                    std::cout << "global path is empty" << std::endl;
+                }
             }else{
                 std::cout << "\033[1A" << "waiting for pose and edge estimation update" << std::endl;
             }
