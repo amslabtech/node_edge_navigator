4953d30a070e161468c7459495305ad51842bb2f
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-09-13 20:36:10 +0900

    update

--- src/local_goal_creator.cpp
+++ src/local_goal_creator.cpp
@@ -3,51 +3,51 @@
 LocalGoalCreator::LocalGoalCreator(void)
 :local_nh("~")
 {
-	local_nh.param("GOAL_DIS", GOAL_DIS, {5.0});
-	local_nh.param("LOCAL_GOAL_ANGLE", LOCAL_GOAL_ANGLE, {M_PI / 3.0});
-	local_nh.param("D_LOCAL_GOAL_ANGLE", D_LOCAL_GOAL_ANGLE, {M_PI / 6.0});
+    local_nh.param("GOAL_DIS", GOAL_DIS, {5.0});
+    local_nh.param("LOCAL_GOAL_ANGLE", LOCAL_GOAL_ANGLE, {M_PI / 3.0});
+    local_nh.param("D_LOCAL_GOAL_ANGLE", D_LOCAL_GOAL_ANGLE, {M_PI / 6.0});
 
-	map_sub = nh.subscribe("/local_map", 1, &LocalGoalCreator::MapCallback, this);
-	target_sub = nh.subscribe("/direction/relative", 1, &LocalGoalCreator::TargetCallback, this);
+    map_sub = nh.subscribe("/local_map", 1, &LocalGoalCreator::MapCallback, this);
+    target_sub = nh.subscribe("/direction/relative", 1, &LocalGoalCreator::TargetCallback, this);
 
-	local_goal_pub = nh.advertise<geometry_msgs::PoseStamped>("/local_goal",1);
+    local_goal_pub = nh.advertise<geometry_msgs::PoseStamped>("/local_goal",1);
 
-	map_received = false;
+    map_received = false;
 }
 
 float LocalGoalCreator::get_yaw(geometry_msgs::Quaternion q)
 {
-	double r, p, y;
-	tf::Quaternion quat(q.x, q.y, q.z, q.w);
-	tf::Matrix3x3(quat).getRPY(r, p,y);
-	return y;
+    double r, p, y;
+    tf::Quaternion quat(q.x, q.y, q.z, q.w);
+    tf::Matrix3x3(quat).getRPY(r, p,y);
+    return y;
 }
 
 void LocalGoalCreator::MapCallback(const nav_msgs::OccupancyGridConstPtr& msg)
 {
-	local_map = *msg;
-	map_received = true;
+    local_map = *msg;
+    map_received = true;
 }
 
 void LocalGoalCreator::TargetCallback(const geometry_msgs::PoseStampedConstPtr& msg)
 {
-	geometry_msgs::PoseStamped target = *msg;
+    geometry_msgs::PoseStamped target = *msg;
 
-	geometry_msgs::PoseStamped local_goal;
-	if(map_received){
-		detection_main(target, local_goal);
-		std::cout << "local goal:" << std::endl;
-		std::cout << local_goal.pose.position << std::endl;
-		local_goal_pub.publish(local_goal);
-	}
-	else{
-		std::cout << "wating for map..." << std::endl;
-	}
+    geometry_msgs::PoseStamped local_goal;
+    if(map_received){
+        detection_main(target, local_goal);
+        std::cout << "local goal:" << std::endl;
+        std::cout << local_goal.pose.position << std::endl;
+        local_goal_pub.publish(local_goal);
+    }
+    else{
+        std::cout << "wating for map..." << std::endl;
+    }
 }
 
 void LocalGoalCreator::detection_main(const geometry_msgs::PoseStamped& target, geometry_msgs::PoseStamped& goal)
 {
-	goal.header = local_map.header;
+    goal.header = local_map.header;
     double target_distance = sqrt(target.pose.position.x * target.pose.position.x + target.pose.position.y * target.pose.position.y);
     std::cout << "target distance: " << target_distance << std::endl;
     double target_orientation = get_yaw(target.pose.orientation);
@@ -56,8 +56,32 @@ void LocalGoalCreator::detection_main(const geometry_msgs::PoseStamped& target, 
     double goal_distance = 0;
     double goal_direction = 1e6;
     std::cout << "ANGLE_SAMPLE_NUM: " << ANGLE_SAMPLE_NUM << std::endl;
-    for(int i=0;i<ANGLE_SAMPLE_NUM;i++){
-        double angle = -LOCAL_GOAL_ANGLE + i * D_LOCAL_GOAL_ANGLE + target_orientation;
+    if(ANGLE_SAMPLE_NUM > 0){
+        for(int i=0;i<ANGLE_SAMPLE_NUM;i++){
+            double angle = -LOCAL_GOAL_ANGLE + i * D_LOCAL_GOAL_ANGLE + target_orientation;
+            std::cout << "angle: " << angle << std::endl;
+            double distance = 0;
+            for(;distance<=target_distance;distance+=local_map.info.resolution){
+                int x_grid = round((distance * cos(angle) - local_map.info.origin.position.x) / local_map.info.resolution);
+                int y_grid = round((distance * sin(angle) - local_map.info.origin.position.y) / local_map.info.resolution);
+                if(local_map.data[x_grid + local_map.info.width * y_grid] != 0){
+                    distance = std::max(0.0, distance - local_map.info.resolution);
+                    break;
+                }
+            }
+            std::cout << "distance: " << distance << std::endl;
+            if(goal_distance <= distance){
+                if(fabs(goal_direction - target_orientation) > fabs(angle - target_orientation)){
+                    goal_distance = distance;
+                    goal_direction = angle;
+                    std::cout << "updated!" << std::endl;
+                    std::cout << "goal_distance: " << goal_distance << std::endl;
+                    std::cout << "goal_direction: " << goal_direction << std::endl;
+                }
+            }
+        }
+    }else{
+        double angle = target_orientation;
         std::cout << "angle: " << angle << std::endl;
         double distance = 0;
         for(;distance<=target_distance;distance+=local_map.info.resolution){
@@ -78,23 +102,24 @@ void LocalGoalCreator::detection_main(const geometry_msgs::PoseStamped& target, 
                 std::cout << "goal_direction: " << goal_direction << std::endl;
             }
         }
+
     }
-	goal.pose.position.x = goal_distance * cos(goal_direction);
-	goal.pose.position.y = goal_distance * sin(goal_direction);
-	goal.pose.position.z = 0.0;
-	goal.pose.orientation = tf::createQuaternionMsgFromYaw(goal_direction);
+    goal.pose.position.x = goal_distance * cos(goal_direction);
+    goal.pose.position.y = goal_distance * sin(goal_direction);
+    goal.pose.position.z = 0.0;
+    goal.pose.orientation = tf::createQuaternionMsgFromYaw(goal_direction);
 }
 
 void LocalGoalCreator::process(void)
 {
-	std::cout << "=== local_goal_creator ===" << std::endl;
-	ros::spin();
+    std::cout << "=== local_goal_creator ===" << std::endl;
+    ros::spin();
 }
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "local_goal_creator");
-	LocalGoalCreator local_goal_creator;
-	local_goal_creator.process();
-	return 0;
+    ros::init(argc, argv, "local_goal_creator");
+    LocalGoalCreator local_goal_creator;
+    local_goal_creator.process();
+    return 0;
 }
