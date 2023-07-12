1657ef7a5aca4aa556d056b6fa8541e3a43ed85b
kosukekosukekosuke <suke_suke_suke@icloud.com>
2023-07-12 12:30:57 +0900

    Change class member variable names

--- include/node_edge_navigator/local_goal_creator.h
+++ include/node_edge_navigator/local_goal_creator.h
@@ -20,65 +20,64 @@
 class LocalGoalCreator
 {
 public:
-
-    /**
+  /**
      * @brief Constructor
      */
-    LocalGoalCreator(void);
+  LocalGoalCreator(void);
 
-    /**
+  /**
      * @brief Beginning of the local goal creator node
      */
-    void process(void);
+  void process(void);
 
-    /**
+  /**
      * @breif Receive a local map (from localmap_creator/src/occupancygrid_integrate.cpp)
      * @param[in] Surrounding obstacle data (2D grid map data [0 or 100 or -1] )
      */
-    void MapCallback(const nav_msgs::OccupancyGridConstPtr&);
+  void MapCallback(const nav_msgs::OccupancyGridConstPtr&);
 
-    /**
+  /**
      * @brief Receive a target node (from node_edge_navigator/src/node_edge_navigator.cpp) , and publish a local goal
      * @param[in] Target node position and orientation data
      */
-    void TargetCallback(const geometry_msgs::PoseStampedConstPtr&);
+  void TargetCallback(const geometry_msgs::PoseStampedConstPtr&);
 
-    /**
+  /**
      * @brief Control the generation of local goal
      * @param[in] Service server request (bool data) and response addresse
      * @param[out] Service server response (true)
      */
-    bool TaskHandler(std_srvs::SetBool::Request&, std_srvs::SetBool::Response&);
+  bool TaskHandler(std_srvs::SetBool::Request&, std_srvs::SetBool::Response&);
 
 private:
-    ros::NodeHandle nh;
-    ros::NodeHandle local_nh;
+  ros::NodeHandle nh_;
+  ros::NodeHandle local_nh_;
 
-    ros::Subscriber map_sub;              // subscriber of local map
-    ros::Subscriber target_sub;           // subscriber of target node(position and orientation)
-    ros::Publisher local_goal_pub;        // publisher of local goal
-    ros::Publisher local_goal_array_pub;  // publisher of local goal array
-    ros::ServiceServer task_server;       // To use the service server, Service servers are used to pass information at any given time.
+  ros::Subscriber map_sub_;              // subscriber of local map
+  ros::Subscriber target_sub_;           // subscriber of target node(position and orientation)
+  ros::Publisher local_goal_pub_;        // publisher of local goal
+  ros::Publisher local_goal_array_pub_;  // publisher of local goal array
+  ros::ServiceServer task_server_;       // To use the service server, Service servers are used to pass information at any given time.
 
-    /**
+  /**
      * @brief Convert a quaternion to a yaw angle
      * @param[in] Quaternion
      * @param[out] Yaw angle
      */
-    float get_yaw(geometry_msgs::Quaternion);
+  float get_yaw(geometry_msgs::Quaternion);
 
-    /**
+  /**
      * @brief Create and update a local goal
      * @param[in] target node, local goal
      */
-    void detection_main(const geometry_msgs::PoseStamped&, geometry_msgs::PoseStamped&);
+  void detection_main(const geometry_msgs::PoseStamped&, geometry_msgs::PoseStamped&);
 
-    nav_msgs::OccupancyGrid local_map;  // Empty box to receive local map
-    bool map_received;                  // Indicates whether a local map is received
-    bool task_flag;                     // Indicates whether the task is being performed
-    double GOAL_DIS;                    // Not used
-    double LOCAL_GOAL_ANGLE;            // Maximum absolute value
-    double D_LOCAL_GOAL_ANGLE;          // Increment
+  nav_msgs::OccupancyGrid local_map_;  // Empty box to receive local map
+  bool map_received_;                  // Indicates whether a local map is received
+  bool task_flag_;                     // Indicates whether the task is being performed
+  double goal_dis_;                    // Not used
+  double local_goal_angle_;            // Maximum absolute value
+  double d_local_goal_angle_;          // Increment
 };
 
 #endif
--- src/local_goal_creator.cpp
+++ src/local_goal_creator.cpp
@@ -1,157 +1,175 @@
 #include "node_edge_navigator/local_goal_creator.h"
 
 LocalGoalCreator::LocalGoalCreator(void)
-:local_nh("~")
+  : local_nh_("~")
 {
-    local_nh.param("GOAL_DIS", GOAL_DIS, {5.0});
-    local_nh.param("LOCAL_GOAL_ANGLE", LOCAL_GOAL_ANGLE, {M_PI / 3.0});
-    local_nh.param("D_LOCAL_GOAL_ANGLE", D_LOCAL_GOAL_ANGLE, {M_PI / 18.0});
+  local_nh_.param("goal_dis", goal_dis_, {5.0});
+  local_nh_.param("local_goal_angle", local_goal_angle_, {M_PI / 3.0});
+  local_nh_.param("d_local_goal_angle", d_local_goal_angle_, {M_PI / 18.0});
 
-    map_sub = nh.subscribe("/local_map", 1, &LocalGoalCreator::MapCallback, this);
-    target_sub = nh.subscribe("/direction/relative", 1, &LocalGoalCreator::TargetCallback, this);
-    task_server = nh.advertiseService("/local_goal/interruption", &LocalGoalCreator::TaskHandler, this);
+  map_sub_ = nh_.subscribe("/local_map", 1, &LocalGoalCreator::MapCallback, this);
+  target_sub_ = nh_.subscribe("/direction/relative", 1, &LocalGoalCreator::TargetCallback, this);
+  task_server_ = nh_.advertiseService("/local_goal/interruption", &LocalGoalCreator::TaskHandler, this);
 
-    local_goal_pub = nh.advertise<geometry_msgs::PoseStamped>("/local_goal",1);
-    local_goal_array_pub = nh.advertise<geometry_msgs::PoseArray>("/local_goal/array", 1);
+  local_goal_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("/local_goal", 1);
+  local_goal_array_pub_ = nh_.advertise<geometry_msgs::PoseArray>("/local_goal/array", 1);
 
-    map_received = false;
-    task_flag = false;
+  map_received_ = false;
+  task_flag_ = false;
 }
 
 float LocalGoalCreator::get_yaw(geometry_msgs::Quaternion q)
 {
-    double r, p, y;
-    tf::Quaternion quat(q.x, q.y, q.z, q.w);
-    tf::Matrix3x3(quat).getRPY(r, p,y);
-    return y;
+  double r, p, y;
+  tf::Quaternion quat(q.x, q.y, q.z, q.w);
+  tf::Matrix3x3(quat).getRPY(r, p, y);
+  return y;
 }
 
 bool LocalGoalCreator::TaskHandler(std_srvs::SetBool::Request& request, std_srvs::SetBool::Response& response)
 {
-    task_flag = request.data;
-    response.success = true;
-    return true;
+  task_flag_ = request.data;
+  response.success = true;
+  return true;
 }
 
 void LocalGoalCreator::MapCallback(const nav_msgs::OccupancyGridConstPtr& msg)
 {
-    local_map = *msg;
-    map_received = true;
+  local_map_ = *msg;
+  map_received_ = true;
 }
 
 void LocalGoalCreator::TargetCallback(const geometry_msgs::PoseStampedConstPtr& msg)
 {
-    geometry_msgs::PoseStamped target = *msg;
+  geometry_msgs::PoseStamped target = *msg;
 
-    geometry_msgs::PoseStamped local_goal;
-    if(map_received && !task_flag){
-        detection_main(target, local_goal);
-        std::cout << "local goal:" << std::endl;
-        std::cout << local_goal.pose.position << std::endl;
-        local_goal_pub.publish(local_goal);
-    }
-    else{
-        std::cout << "wating for map..." << std::endl;
-    }
+  geometry_msgs::PoseStamped local_goal;
+  if (map_received_ && !task_flag_)
+  {
+    detection_main(target, local_goal);
+    std::cout << "local goal:" << std::endl;
+    std::cout << local_goal.pose.position << std::endl;
+    local_goal_pub_.publish(local_goal);
+  }
+  else
+  {
+    std::cout << "wating for map..." << std::endl;
+  }
 }
 
 void LocalGoalCreator::detection_main(const geometry_msgs::PoseStamped& target, geometry_msgs::PoseStamped& goal)
 {
-    goal.header = local_map.header;
-    double target_distance = sqrt(target.pose.position.x * target.pose.position.x + target.pose.position.y * target.pose.position.y);
-    std::cout << "target distance: " << target_distance << std::endl;
+  goal.header = local_map_.header;
+  double target_distance = sqrt(target.pose.position.x * target.pose.position.x + target.pose.position.y * target.pose.position.y);
+  std::cout << "target distance: " << target_distance << std::endl;
 
-    double target_orientation = get_yaw(target.pose.orientation);
-    std::cout << "target direction\n" << target_orientation << std::endl;
+  double target_orientation = get_yaw(target.pose.orientation);
+  std::cout << "target direction\n"
+            << target_orientation << std::endl;
 
-    const int ANGLE_SAMPLE_NUM = 2 * round(LOCAL_GOAL_ANGLE / D_LOCAL_GOAL_ANGLE) + 1;  // Number of samples to see obstacles in the vicinity, 2*(60/10)+1 = 13
-    double goal_distance = 0;
-    double goal_direction = 1e6;
-    std::cout << "ANGLE_SAMPLE_NUM: " << ANGLE_SAMPLE_NUM << std::endl;
+  const int ANGLE_SAMPLE_NUM = 2 * round(local_goal_angle_ / d_local_goal_angle_) + 1;  // Number of samples to see obstacles in the vicinity, 2*(60/10)+1 = 13
+  double goal_distance = 0;
+  double goal_direction = 1e6;
+  std::cout << "ANGLE_SAMPLE_NUM: " << ANGLE_SAMPLE_NUM << std::endl;
 
-    if(ANGLE_SAMPLE_NUM > 0){
-        geometry_msgs::PoseArray local_goal_array;  // local_goal_array = Pose[] (Pose is position and Quaternion)
-        local_goal_array.header = goal.header;      // goal = local_goal
-        for(int i=0;i<ANGLE_SAMPLE_NUM;i++){        // Checking the distance to obstacles around the target node, Repeat until i=12
-            double angle = -LOCAL_GOAL_ANGLE + i * D_LOCAL_GOAL_ANGLE + target_orientation;  // -60 + i*10 + target_node
-            std::cout << "angle: " << angle << std::endl;
-            double distance = 0;
-            for(;distance<=target_distance;distance+=local_map.info.resolution){             // Distance is defined as the distance to 1grid before the obstacle
-                int x_grid = round((distance * cos(angle) - local_map.info.origin.position.x) / local_map.info.resolution);
-                int y_grid = round((distance * sin(angle) - local_map.info.origin.position.y) / local_map.info.resolution);
-                if(local_map.data[x_grid + local_map.info.width * y_grid] != 0){
-                    distance = std::max(0.0, distance - local_map.info.resolution);
-                    break;
-                }
-            }
-
-            geometry_msgs::Pose p;
-            p.position.x = distance * cos(angle);
-            p.position.y = distance * sin(angle);
-            p.orientation = tf::createQuaternionMsgFromYaw(angle);  // Create quaternion from yaw data
-            local_goal_array.poses.push_back(p);                    // Put p from behind the local goal array
-            std::cout << "distance: " << distance << std::endl;
-
-            // Updating local goal, Initial goal_distance is 0
-            if(goal_distance < distance){  // Make goal_distance longer
-                goal_distance = distance;
-                goal_direction = angle;
-                std::cout << "updated!" << std::endl;
-                std::cout << "goal_distance: " << goal_distance << std::endl;
-                std::cout << "goal_direction: " << goal_direction << std::endl;
-            }else if(goal_distance == distance){
-                if(fabs(goal_direction - target_orientation) > fabs(angle - target_orientation)){  // Make goal_direction smaller
-                    goal_direction = angle;
-                    std::cout << "updated!" << std::endl;
-                    std::cout << "goal_distance: " << goal_distance << std::endl;
-                    std::cout << "goal_direction: " << goal_direction << std::endl;
-                }
-            }
+  if (ANGLE_SAMPLE_NUM > 0)
+  {
+    geometry_msgs::PoseArray local_goal_array;  // local_goal_array = Pose[] (Pose is position and Quaternion)
+    local_goal_array.header = goal.header;      // goal = local_goal
+    for (int i = 0; i < ANGLE_SAMPLE_NUM; i++)
+    {                                                                                  // Checking the distance to obstacles around the target node, Repeat until i=12
+      double angle = -local_goal_angle_ + i * d_local_goal_angle_ + target_orientation;  // -60 + i*10 + target_node
+      std::cout << "angle: " << angle << std::endl;
+      double distance = 0;
+      for (; distance <= target_distance; distance += local_map_.info.resolution)
+      {  // Distance is defined as the distance to 1grid before the obstacle
+        int x_grid = round((distance * cos(angle) - local_map_.info.origin.position.x) / local_map_.info.resolution);
+        int y_grid = round((distance * sin(angle) - local_map_.info.origin.position.y) / local_map_.info.resolution);
+        if (local_map_.data[x_grid + local_map_.info.width * y_grid] != 0)
+        {
+          distance = std::max(0.0, distance - local_map_.info.resolution);
+          break;
         }
-        local_goal_array_pub.publish(local_goal_array);  // local goal array is also published
-    }else{                                               // ANGLE_SAMPLE_NUM <= 0, This else statement is not much different from the above
-        double angle = target_orientation;
-        std::cout << "angle: " << angle << std::endl;
+      }
 
-        double distance = 0;
+      geometry_msgs::Pose p;
+      p.position.x = distance * cos(angle);
+      p.position.y = distance * sin(angle);
+      p.orientation = tf::createQuaternionMsgFromYaw(angle);  // Create quaternion from yaw data
+      local_goal_array.poses.push_back(p);                    // Put p from behind the local goal array
+      std::cout << "distance: " << distance << std::endl;
 
-        for(;distance<=target_distance;distance+=local_map.info.resolution){
-            int x_grid = round((distance * cos(angle) - local_map.info.origin.position.x) / local_map.info.resolution);
-            int y_grid = round((distance * sin(angle) - local_map.info.origin.position.y) / local_map.info.resolution);
-            if(local_map.data[x_grid + local_map.info.width * y_grid] != 0){
-                distance = std::max(0.0, distance - local_map.info.resolution);
-                break;
-            }
+      // Updating local goal, Initial goal_distance is 0
+      if (goal_distance < distance)
+      {  // Make goal_distance longer
+        goal_distance = distance;
+        goal_direction = angle;
+        std::cout << "updated!" << std::endl;
+        std::cout << "goal_distance: " << goal_distance << std::endl;
+        std::cout << "goal_direction: " << goal_direction << std::endl;
+      }
+      else if (goal_distance == distance)
+      {
+        if (fabs(goal_direction - target_orientation) > fabs(angle - target_orientation))
+        {  // Make goal_direction smaller
+          goal_direction = angle;
+          std::cout << "updated!" << std::endl;
+          std::cout << "goal_distance: " << goal_distance << std::endl;
+          std::cout << "goal_direction: " << goal_direction << std::endl;
         }
-        std::cout << "distance: " << distance << std::endl;
-
-        if(goal_distance <= distance){
-            if(fabs(goal_direction - target_orientation) > fabs(angle - target_orientation)){
-                goal_distance = distance;
-                goal_direction = angle;
-                std::cout << "updated!" << std::endl;
-                std::cout << "goal_distance: " << goal_distance << std::endl;
-                std::cout << "goal_direction: " << goal_direction << std::endl;
-            }
-        }
+      }
     }
+    local_goal_array_pub_.publish(local_goal_array);  // local goal array is also published
+  }
+  else
+  {  // ANGLE_SAMPLE_NUM <= 0, This else statement is not much different from the above
+    double angle = target_orientation;
+    std::cout << "angle: " << angle << std::endl;
 
-    goal.pose.position.x = goal_distance * cos(goal_direction);
-    goal.pose.position.y = goal_distance * sin(goal_direction);
-    goal.pose.position.z = 0.0;
-    goal.pose.orientation = tf::createQuaternionMsgFromYaw(goal_direction);
+    double distance = 0;
+
+    for (; distance <= target_distance; distance += local_map_.info.resolution)
+    {
+      int x_grid = round((distance * cos(angle) - local_map_.info.origin.position.x) / local_map_.info.resolution);
+      int y_grid = round((distance * sin(angle) - local_map_.info.origin.position.y) / local_map_.info.resolution);
+      if (local_map_.data[x_grid + local_map_.info.width * y_grid] != 0)
+      {
+        distance = std::max(0.0, distance - local_map_.info.resolution);
+        break;
+      }
+    }
+    std::cout << "distance: " << distance << std::endl;
+
+    if (goal_distance <= distance)
+    {
+      if (fabs(goal_direction - target_orientation) > fabs(angle - target_orientation))
+      {
+        goal_distance = distance;
+        goal_direction = angle;
+        std::cout << "updated!" << std::endl;
+        std::cout << "goal_distance: " << goal_distance << std::endl;
+        std::cout << "goal_direction: " << goal_direction << std::endl;
+      }
+    }
+  }
+
+  goal.pose.position.x = goal_distance * cos(goal_direction);
+  goal.pose.position.y = goal_distance * sin(goal_direction);
+  goal.pose.position.z = 0.0;
+  goal.pose.orientation = tf::createQuaternionMsgFromYaw(goal_direction);
 }
 
 void LocalGoalCreator::process(void)
 {
-    std::cout << "=== local_goal_creator ===" << std::endl;
-    ros::spin();
+  std::cout << "=== local_goal_creator ===" << std::endl;
+  ros::spin();
 }
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "local_goal_creator");
-    LocalGoalCreator local_goal_creator;
-    local_goal_creator.process();
-    return 0;
+  ros::init(argc, argv, "local_goal_creator");
+  LocalGoalCreator local_goal_creator;
+  local_goal_creator.process();
+  return 0;
 }
+
